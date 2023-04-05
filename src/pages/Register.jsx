f5ed1a12c9e753e2c1f8c75e3dc90423bdd1d9a5
navpannu32 <pannunav74@gmail.com>
2023-04-05 04:08:31 -0500

    updated Register.jsx
    updated to connect backend to add regiser functionality to frontend

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { redirect } from "react-router-dom";
 import "./Register.css";
 
 const Register = () => {
@@ -23,8 +24,36 @@ const Register = () => {
     setConfirmPassword(event.target.value);
   };
 
-  const onSubmit = (event) => {
+  const onSubmit = async (event) => {
     event.preventDefault();
+
+    if (password === confirmPassword) {
+      const response = await fetch(
+        "http://localhost/restaurants/api/users/create.php",
+        {
+          method: "POST",
+          mode: "no-cors",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            name,
+            email,
+            password,
+            role: "user",
+          }),
+        }
+      );
+
+      console.log("response: ", response);
+    }
+
+    setName("");
+    setEmail("");
+    setPassword("");
+    setConfirmPassword("");
+
+    redirect("/login");
   };
 
   return (
@@ -36,6 +65,7 @@ const Register = () => {
           name="name"
           onChange={handleNameChange}
           value={name}
+          required
         />
       </div>
       <div className="input-group">
@@ -45,6 +75,7 @@ const Register = () => {
           name="email"
           onChange={handleEmailChange}
           value={email}
+          required
         />
       </div>
       <div className="input-group">
@@ -54,6 +85,7 @@ const Register = () => {
           name="password"
           onChange={handlePasswordChange}
           value={password}
+          required
         />
       </div>
       <div className="input-group">
@@ -63,6 +95,7 @@ const Register = () => {
           name="confirmPassword"
           onChange={handleConfirmPasswordChange}
           value={confirmPassword}
+          required
         />
       </div>
       <input type="submit" />
