439330dae74c9bc47555e481b19c1ba67ad8f571
navpannu32 <pannunav74@gmail.com>
2023-04-07 04:34:48 -0500

    changed url to access register page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import Navbar from "./components/Navbar";
+import Items from "./components/Items";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Navbar />
         <Routes>
+          <Route path="/" element={<Items />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
+          <Route path="/signup" element={<Register />} />
         </Routes>
       </Router>
     </div>
